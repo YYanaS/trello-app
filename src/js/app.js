8fc4571d14a11e8524efc6ecca8355339c014b0c
YYanaS <yanayazuchno@gmail.com>
2026-01-17 13:41:06 +0300

    init: application initialization

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+// ОБЯЗАТЕЛЬНО: импорт стилей (чтобы Parcel их собрал)
+import '../scss/app.scss';
+
+// Импорт Bootstrap компонентов
+import { Modal, Dropdown } from 'bootstrap';
+
+// Ваш JS код
+console.log('App loaded');
